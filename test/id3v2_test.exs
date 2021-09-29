c729651984dd7f3af0e64e96903211556022909b
cmush <collinsmucheru@gmail.com>
2021-09-30 01:04:18 +0300

    chore: add sample mp3 file update .gitignore

--- test/id3v2_test.exs
+++ test/id3v2_test.exs
@@ -1,18 +1,18 @@
 defmodule ID3Test do
   use ExUnit.Case
   # doctest ID3
+  @path_to_sample_mp3 "priv/sample_mp3/WAKADINALI-Morio-Anzenza-Ft-Dyana-Cods.mp3"
 
   describe "get_tag" do
     test "get_tag!/1: return ID3_struct when valid mp3 path provided" do
-      assert %ID3.Tag{} =
-               ID3.get_tag!("/Users/cmush/Downloads/WAKADINALI-Morio-Anzenza-Ft-Dyana-Cods.mp3")
+      assert %ID3.Tag{} = ID3.get_tag!(@path_to_sample_mp3)
     end
 
     test "get_tag/1: return {:ok, ID3_struct} when valid mp3 path provided" do
       assert {
                :ok,
                %ID3.Tag{}
-             } = ID3.get_tag("/Users/cmush/Downloads/WAKADINALI-Morio-Anzenza-Ft-Dyana-Cods.mp3")
+             } = ID3.get_tag(@path_to_sample_mp3)
     end
 
     test "get_tag/1: return error tuple when invalid path provided" do
