7027b1d8da8eabfd3f64da9b1bef3954f995216a
cmush <collinsmucheru@gmail.com>
2021-09-30 00:57:46 +0300

    test: get_tag!/1 and get_tag/1

--- test/id3v2_test.exs
+++ test/id3v2_test.exs
@@ -1,8 +1,22 @@
 defmodule ID3Test do
   use ExUnit.Case
-  doctest ID3
+  # doctest ID3
 
-  test "greets the world" do
-    assert ID3.hello() == :world
+  describe "get_tag" do
+    test "get_tag!/1: return ID3_struct when valid mp3 path provided" do
+      assert %ID3.Tag{} =
+               ID3.get_tag!("/Users/cmush/Downloads/WAKADINALI-Morio-Anzenza-Ft-Dyana-Cods.mp3")
+    end
+
+    test "get_tag/1: return {:ok, ID3_struct} when valid mp3 path provided" do
+      assert {
+               :ok,
+               %ID3.Tag{}
+             } = ID3.get_tag("/Users/cmush/Downloads/WAKADINALI-Morio-Anzenza-Ft-Dyana-Cods.mp3")
+    end
+
+    test "get_tag/1: return error tuple when invalid path provided" do
+      assert {:error, :file_open_error} = ID3.get_tag("")
+    end
   end
 end
