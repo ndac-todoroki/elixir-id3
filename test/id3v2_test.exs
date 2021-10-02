e7b581c0efe1d6ec3469a7a808d680dd8556e07a
cmush <collinsmucheru@gmail.com>
2021-10-02 15:58:47 +0300

    test: write_tag include test mp3 file

--- test/id3v2_test.exs
+++ test/id3v2_test.exs
@@ -19,4 +19,37 @@ defmodule ID3Test do
       assert {:error, :file_open_error} = ID3.get_tag("")
     end
   end
+
+  describe "write_tag" do
+    test "write_tag/2" do
+      # get file with year: nil
+      assert {
+               :ok,
+               %ID3.Tag{
+                 year: nil
+               } = tag
+             } = ID3.get_tag(@path_to_sample_mp3)
+
+      # write tag year: 2020
+      ID3.write_tag(%{tag | year: 2020}, @path_to_sample_mp3)
+      # assert tag year: 2020
+      assert {
+               :ok,
+               %ID3.Tag{year: 2020} = tag
+             } = ID3.get_tag(@path_to_sample_mp3)
+
+      on_exit(fn ->
+        IO.puts(
+          "#{__MODULE__}.on_exit/1 cleaning up test: write_tag/2 -> resetting test file to initial tag"
+        )
+
+        ID3.write_tag(%{tag | year: nil}, @path_to_sample_mp3)
+
+        assert {
+                 :ok,
+                 %ID3.Tag{year: nil}
+               } = ID3.get_tag(@path_to_sample_mp3)
+      end)
+    end
+  end
 end
